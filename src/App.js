021b2d18360a66fbb93003766853afae46ec43dc
Denis Kukhtyn <denis.kukhtyn@nixsolutions.com>
2020-05-30 13:35:59 +0300

    Header added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.scss';
+import { Header } from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <header className="App-header">
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
