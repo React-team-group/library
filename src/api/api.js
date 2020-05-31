91939c2e5e57510c7f5f12df3cd6cc39b5bcfb1a
Aleksandr Troianovskii <aleksandr.troianovskii@nixsolutions.com>
2020-05-31 12:44:13 +0300

    add get and post request

--- src/api/api.js
+++ src/api/api.js
@@ -1,16 +1,30 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://openlibrary.org/api',
+  baseURL: 'http://localhost:4000',
   headers: { 'Content-Type': 'application/json' },
 });
 
 export const HttpService = {
-  getBooksIsBn: (num) => {
-    return instance.get(`/books?bibkeys=${num}`).then((res) => {
-      console.log(res);
-    });
+  getProducts: (page = 1, limit = 10) => {
+    return instance.get(`/products?_page=${page}&_limit=${limit}`).then((res) => res);
+  },
+
+  getProductId: (id) => {
+    return instance.get(`/products/${id}`).then((res) => res);
+  },
+
+  getCategories: () => {
+    return instance.get(`/categories`).then((res) => res);
+  },
+
+  postOrder: (data) => {
+    return instance
+      .post(`/orders`, data, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+      .then((res) => res);
   },
 };
-
-HttpService.getBooks();
